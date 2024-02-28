85cbef15d45f6be89dff09e0eb2c557622dfc704
nhan <nhannn87dn@gmail.com>
2024-02-28 08:39:40 +0700

    day7

---
+++ Batch-38/Session-10.sql
@@ -0,0 +1,111 @@
+﻿CREATE VIEW dbo.v_daily_sales
+AS
+SELECT
+    p.product_id,
+    p.product_name,
+    p.discount,
+    year(order_date) AS y,
+    month(order_date) AS m,
+    day(order_date) AS d,
+    (i.quantity * i.price) AS sales
+FROM
+    dbo.orders AS o
+INNER JOIN dbo.order_items AS i
+    ON o.order_id = i.order_id
+INNER JOIN dbo.products AS p
+    ON p.product_id = i.product_id
+
+SELECT * FROM dbo.products WHERE product_id = 8
+SELECT * FROM dbo.v_daily_sales
+
+UPDATE dbo.v_daily_sales SET discount=0.04 WHERE product_id = 8
+
+EXEC sp_helptext [v_daily_sales];
+
+--PR
+--Sử dụng từ khóa CREATE PROCEDURE
+ALTER PROCEDURE usp_ProductList
+AS
+BEGIN
+    BEGIN TRY
+        SELECT 
+            product_name, 
+            price
+        FROM 
+            dbo.products
+        ORDER BY 
+            product_name;
+    END TRY
+    BEGIN CATCH
+        -- Nếu có lỗi xảy ra, hiển thị thông tin lỗi
+        SELECT 
+            ERROR_NUMBER() AS ErrorNumber,
+            ERROR_MESSAGE() AS ErrorMessage;
+        --Ném lỗi
+        THROW;
+    END CATCH;
+END;
+
+EXECUTE usp_ProductList
+
+--PROC có tham số đầu vào như Function
+ALTER PROCEDURE usp_FindProductsByModelYear(@model_year INT)
+AS
+BEGIN
+    BEGIN TRY
+        SELECT
+            product_name,
+			model_year,
+            price
+        FROM 
+            dbo.products
+        WHERE
+            model_year >= @model_year
+        ORDER BY
+            price;
+    END TRY
+    BEGIN CATCH
+        -- Nếu có lỗi xảy ra, hiển thị thông tin lỗi
+        SELECT 
+            ERROR_NUMBER() AS ErrorNumber,
+            ERROR_MESSAGE() AS ErrorMessage;
+        --Ném lỗi
+        THROW;
+    END CATCH;
+END;
+--Sử dụng Store khi có tham số
+EXECUTE usp_FindProductsByModelYear 2019;
+
+
+CREATE PROCEDURE usp_TotalOrderByRangeDate (
+    @FromDate DATETIME, --tham số đầu vào
+    @ToDate DATETIME, --tham số đầu vào
+    @Total INT OUTPUT --Tham số đầu ra OUTPUT
+)
+AS
+BEGIN
+  SELECT @Total = COUNT(*) FROM orders WHERE CAST(order_date AS DATE)  BETWEEN @FromDate AND @ToDate
+END;
+
+DECLARE @TotalOrders INT;
+
+EXEC usp_TotalOrderByRangeDate '2016-01-01', '2016-12-30', @TotalOrders OUTPUT;
+
+SELECT @TotalOrders as TotalOrders;
+
+SELECT * FROM orders
+
+
+CREATE PROCEDURE usp_CheckOrderStatus(@OrderId INT)
+AS
+BEGIN
+    IF EXISTS(SELECT 1 FROM dbo.orders WHERE order_id = @OrderId)
+        RETURN 1 -- Order exists
+    ELSE
+        RETURN 0 -- Order does not exist
+END;
+
+DECLARE @Status INT
+EXEC @Status = usp_CheckOrderStatus 1
+SELECT @Status as Status
+
---
+++ Batch-38/Session-12.sql
@@ -0,0 +1,66 @@
+﻿
+CREATE TRIGGER trg_OrderItems_Update_ProductStock
+ON order_items
+AFTER INSERT
+AS
+BEGIN
+    BEGIN TRY
+        UPDATE stocks
+            SET quantity = s.quantity - i.quantity
+        FROM
+        stocks as s
+        INNER JOIN inserted AS i ON s.product_id = i.product_id
+        INNER JOIN orders AS o ON o.order_id = i.order_id AND o.store_id = s.store_id;
+    END TRY
+    BEGIN CATCH
+        -- Nếu có lỗi xảy ra, hiển thị thông tin lỗi
+        SELECT 
+            ERROR_NUMBER() AS ErrorNumber,
+            ERROR_MESSAGE() AS ErrorMessage;
+		THROW;
+    END CATCH
+END;
+SELECT price, discount FROM dbo.products WHERE product_id = 1
+
+SELECT * FROM order_items
+SELECT * FROM orders WHERE order_id = 1615
+
+INSERT 
+	order_items
+(order_id, item_id, product_id,quantity,price,discount)
+VALUES
+(1615,4,1,1,379.99,3.00)
+--xem tồn kho trước
+--storeIid=3
+--quanlity=14
+SELECT * FROM stocks WHERE product_id = 1
+
+SELECT * FROM staffs
+
+
+CREATE TRIGGER trg_brands_PreventInsert
+ON brands
+INSTEAD OF INSERT
+AS
+BEGIN
+    PRINT 'Cannot insert data into the Customers table.'
+END
+
+INSERT brands
+(brand_name)
+VALUES
+('BMW')
+
+SELECT * FROM brands
+
+CREATE TRIGGER trg_customers_Prevent_DropTable
+ON DATABASE
+FOR DROP_TABLE
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[customers]') AND type in (N'U'))
+    BEGIN
+        PRINT 'Cannot drop the table: Customers.'
+        ROLLBACK
+    END
+END;
