224dbd7b21e6a029e918d6f09bfc9ed288dbe532
nhannn87dn <nhannn87dn@gmail.com>
2024-11-14 20:33:45 +0700

    up

---
+++ Batch-190/ss6.sql
@@ -0,0 +1,159 @@
+﻿USE Batch190
+
+-- Để lấy tất cả dữ liệu của một table
+SELECT * FROM categories
+-- Chèn 1 dòng/row/record vào một table
+INSERT INTO categories (
+	category_id,
+	category_name,
+	description
+)
+VALUES (
+	3,
+	'Tablet',
+	'Tablet gia re co ban tra gop'
+)
+
+INSERT INTO categories (
+	category_id,
+	category_name
+	
+)
+VALUES (
+	4,
+	'Tablet v2'
+)
+-- Chèn nhiều dòng cùng lúc
+INSERT INTO categories 
+	(category_id,category_name)
+VALUES 
+	(5,'Tablet v5'),
+	(6,'Tablet v6'),
+	(7,'Tablet v7'),
+	(8,'Tablet v8')
+
+
+CREATE TABLE [dbo].[myTable] (
+    Id              INT,
+    Age             TINYINT,
+    Price           DECIMAL(18,2),
+    Discount        DECIMAL(4,2),
+    ProductName     NVARCHAR(255),
+    BirthDay        DATE, --Kiểu ngày yyyy-mm-dd
+    CreatedAt       DATETIME2, --kiểu yyyy-mm-dd H:i:s
+    StartAt         TIME (0), -- H:i:s
+    EndAt           TIME (0), -- H:i:s
+    IsActive        BIT, -- 0 or 1
+    Content         NVARCHAR(MAX), --Nội dung dài
+    ModuleId        UNIQUEIDENTIFIER DEFAULT NEWID()
+)
+
+INSERT INTO [dbo].[myTable]
+(Id, Age, Price, Discount, ProductName, BirthDay, CreatedAt, StartAt, EndAt, IsActive, Content, ModuleId)
+VALUES
+(1, 30, 150.00, 0.10, N'Sản phẩm A', '2000-01-01', '2024-04-03 08:32:34', '08:00:00', '17:00:00', 1, N'Nội dung sản phẩm A', NEWID());
+
+--- CHÈN dữ liệu vào một table có khóa chính
+/* IDENTITY (param1, param 2)
+	param1: là giá trị khởi tạo
+	param2: bước nhảy
+*/
+CREATE TABLE dbo.brands (
+	brand_id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	brand_name NVARCHAR(50) NOT NULL,
+	description NVARCHAR(255) NULL
+)
+
+CREATE TABLE dbo.brands (
+	brand_id INT IDENTITY(1,1)  NOT NULL,
+	brand_name NVARCHAR(50) NOT NULL,
+	description NVARCHAR(255) NULL
+	-- PK_brands_brand_id là tên mà đặt cho khóa chính
+	CONSTRAINT PK_brands_brand_id PRIMARY KEY (brand_id)
+)
+
+
+SELECT * FROM dbo.brands
+
+SET IDENTITY_INSERT dbo.brands ON;
+INSERT dbo.brands 
+	(brand_name) 
+VALUES
+	(N'Xiaomi 1'),
+	(N'Xiaomi 2'),
+	(N'Xiaomi 3')
+
+SET IDENTITY_INSERT dbo.brands OFF;
+
+INSERT dbo.brands 
+	(brand_name) 
+VALUES
+	(N'Samsung'),
+	(N'Samsung 1'),
+	(N'Samsung 2')
+
+--- lệnh UPDATE dữ liệu
+UPDATE 
+	dbo.brands 
+SET 
+	description = N'Samsung của hàn quốc 8',
+	brand_name = N'Samsung 8'
+WHERE 
+	brand_id = 8
+
+-- lệnh xóa dữ liệu trong bảng
+DELETE dbo.brands -- Xóa tất cả các dòng dữ liệu có trong bảng brand
+DELETE dbo.brands WHERE brand_id = 1 -- xóa 1 dòng nơi có brand_id = 1 thôi
+TRUNCATE TABLE dbo.brands -- Xóa tất cả các dòng dữ liệu có trong bảng brand + reset tự tăng
+
+
+-- Lấy dữ liệu từ brands đổ vào cho brands_bk
+SET IDENTITY_INSERT dbo.brands_bk ON;
+INSERT INTO dbo.brands_bk (brand_id, brand_name, description)
+SELECT brand_id, brand_name, description FROM dbo.brands
+SET IDENTITY_INSERT dbo.brands_bk OFF;
+
+SELECT * FROM brands
+SELECT * FROM brands_bk
+
+--thêm khóa chính cho tablet có sẳn
+ALTER TABLE dbo.categories
+ADD CONSTRAINT PK_categories_category_id PRIMARY KEY (category_id);
+
+SELECT * FROM categories
+DELETE FROm categories WHERE category_id = 3
+
+-- XÓA KHÓA CHÍNH ĐÃ TẠO	
+ALTER TABLE categories
+DROP CONSTRAINT PK_categories_category_id
+
+-- Thiết lập khóa ngoại cho product có quan hệ MANY T0 ONE với brand
+CREATE TABLE dbo.products (
+	product_id INT IDENTITY(1,1) NOT NULL,
+	product_name NVARCHAR(255) UNIQUE NOT NULL, -- UNIQUE chống trùng tên
+	price DECIMAL(18,2) DEFAULT 0 CHECK (price >= 0), --mặc định nếu ko truyền thì giá = 0
+	discount DECIMAL(4,2) CHECK (discount >=0 AND discount <= 49), --99,99%
+	CONSTRAINT PK_products_product_id PRIMARY KEY (product_id),
+	--nó có quan hệ với brand --> xác định sp này thuộc thương hiệu nào
+	brand_id INT NOT NULL, -- test chưa thiết lập khóa ngoại
+)
+TRUNCATE TABLE [dbo].[products]
+-- them khoa ngoai cho table co san
+ALTER TABLE [dbo].[products]
+ADD CONSTRAINT [FK_products_brands_id] FOREIGN KEY ([brand_id]) 
+	REFERENCES [dbo].[brands] ([brand_id])
+	ON DELETE SET NULL --
+
+ALTER TABLE [dbo].[products]
+ALTER COLUMN brand_id INT NULL
+-- giúp đảm bảo tính toàn vẹn dữ liệu
+-- BUT truy vấn sẽ bị chậm đi
+
+INSERT dbo.products
+(product_name, price, discount, brand_id)
+VALUES
+(N'Xiaomi Readmi Note 15', 2000, 30, 5)
+
+SELECT * FROM products
+SELECT * FROM brands
+DELETE brands WHERE brand_id= 5
---
+++ Batch-190/ss8.sql
@@ -0,0 +1,155 @@
+﻿-- 1. select without from
+SELECT 
+    LOWER('TEST') result;
+
+SELECT RAND() as result;
+
+-- 2. select with from
+-- 2.1 Lấy tất cả dữ liệu của table brands
+SELECT * FROM dbo.brands -- dấu * dại diện tất cả các cột của table đang có
+-- 2.2 Lấy một trường cụ thể của table --> liệt kê các trường cần lấy, 
+-- cách nhau = dấu phẩy
+SELECT product_id, product_name, price FROM dbo.products
+
+-- 2.3 Select với một biểu thức
+SELECT (2 + 5) As total;
+SELECT
+	customer_id AS id, -- id thôi
+	(first_name + ' ' + last_name) AS full_name
+FROM 
+	dbo.customers
+-- 2.4  SELECT với DISTINCT
+-- Liệt kê danh sách các bang (state) trong dữ liệu customers
+SELECT 
+DISTINCT state -- DISTINCT loại bỏ các dòng có giá trị trùng lặp
+FROM
+	dbo.customers
+SELECT 
+DISTINCT shipping_city 
+FROM 
+orders
+-- 2.5 SELECT với mệnh đề TOP & TOP PERCENT
+-- Lấy 5 sp trong danh sách sản phẩm
+SELECT
+TOP 5
+  *
+FROM
+	dbo.products
+-- Lấy 5% số dòng dữ liệu của table products đang có
+SELECT
+TOP 5 PERCENT -- = 5%
+  *
+FROM
+	dbo.products
+
+
+-- 2.6 SELECT với mệnh đề WITH TIES
+-- 2.7 SELECT với mệnh đề WHERE
+-- 2.7.1 Chỉ lấy những sp có model_year năm 2018
+SELECT 
+	*
+FROM dbo.products
+WHERE model_year = 2018 -- TOÁN TỬ SO SÁNH =
+-- 2.7.2  Lấy những sp có model year > 2022
+SELECT 
+	*
+FROM dbo.products
+WHERE model_year > 2022
+-- 2.7.3 SELECT với TOÁN TỬ AND (Kết hợp nhiều điều kiện)
+-- Lấy những sp có category_id = 2 và brand_id = 7
+SELECT * FROM dbo.products
+WHERE category_id = 2 AND brand_id = 7 -- Thõa mản tất các vế của AND
+-- 2.7.4  SELLECT với TOÁN TỬ OR (Hoặc = 1 trong các điều kiện)
+-- Liệt kê dssp có model_year = 2025 hoặc 2028
+SELECT * FROM dbo.products
+WHERE model_year = 2025 OR model_year = 2028
+-- 2.7.5  SELLECT với TOÁN TỬ IN (1 trong các điều kiện == OR)
+SELECT * FROM dbo.products
+WHERE model_year IN (2025, 2028) -- tương đương với OR ở 2.7.4
+-- 2.7.6  SELLECT với BETWEEN
+-- Liệt kê DSSP có năm sx (model_year) từ 2025-2028
+SELECT * FROM dbo.products
+WHERE model_year >= 2025 AND model_year <= 2028
+SELECT * FROM dbo.products
+WHERE model_year BETWEEN 2025 AND 2028
+
+-- 2.7.7  SELECT với TOÁN TỬ LIKE
+-- Tìm all sản phẩm mà tên có chứa từ 'Sun'
+SELECT
+ *
+FROM dbo.products
+WHERE product_name LIKE '%sun%'
+
+SELECT
+ *
+FROM dbo.products
+WHERE product_name LIKE 'sun%' -- bắt đầu là sun
+
+SELECT
+ *
+FROM dbo.products
+WHERE product_name LIKE '%sun' -- kết thúc là sun
+
+-- Tìm tất cả khách có số điện thoại có số đuôi là 553
+SELECT * FROM dbo.customers
+WHERE phone LIKE '%553'
+
+-- 2.7.8  SELECT với kiểu dữ liệu ngày tháng
+-- Tìm tất cả khách hàng có sinh nhật tháng 9
+SELECT MONTH(CAST('2016-06-01' AS DATE)) as m
+SELECT *
+FROM dbo.customers
+WHERE MONTH(birthday)= 9
+-- Tìm tất cả đơn hàng bán ra từ ngày đến ngày
+SELECT 
+*
+FROM orders
+WHERE 
+	order_date >= CAST('2016-01-01' AS DATE) AND order_date <= CAST('2016-01-30' AS DATE)
+	-- Tìm tất cả đơn hàng bán ra năm 2018
+
+-- 2.7.8 SELECT với mệnh đề ORDER BY
+-- Dùng để sắp xếp chiều tăng dần, giảm dần
+SELECT * FROM dbo.products
+ORDER BY product_name ASC, price ASC -- tăng dần
+
+SELECT * FROM dbo.products
+ORDER BY price	DESC -- giảm dần
+
+-- 2.7.9  SELECT với mệnh đề OFFSET-FETCH --> phân trang
+-- Mục đích của phân trang --> giảm tải cho server
+
+SELECT
+	product_id,product_name,price
+FROM
+    dbo.products
+ORDER BY product_id
+OFFSET 10 ROWS  --  OFFSET = vị trí lấy
+FETCH NEXT 10 ROWS ONLY; -- mỗi lần lấy 10
+
+-- 2.7.10  SELECT với GROUP BY
+-- Thống kê xem có bn khách hàng ở mỗi bang
+
+SELECT 
+DISTINCT state -- DISTINCT loại bỏ các dòng có giá trị trùng lặp
+FROM
+	dbo.customers
+
+SELECT 
+	state,
+	COUNT(customer_id) -- đếm số lượng trên group đã nhóm
+FROM
+	dbo.customers
+GROUP BY -- khử trùng lặp, cho phép tính toán trên nhóm
+	state
+-- Thống kê số tiền đã mua của mỗi khách hàng ?
+SELECT * FROM orders WHERE order_id = 1
+
+SELECT 
+ order_id,
+ SUM(quantity * price)
+FROM order_items WHERE order_id = 1
+GROUP BY order_id
+
+
+SELECT * FROM order_items
