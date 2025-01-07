0dbb22685d7d040ea00cd90efb44572f3336a439
nhannn87dn <nhannn87dn@gmail.com>
2025-01-07 20:19:59 +0700

    add ws ss6

---
+++ Batch-40/workshop-ss-6.sql
@@ -0,0 +1,10 @@
+﻿/* 
+Hãy tạo một database quản lý khách sạn:
+- Customers (quản lý khách hàng)
+- Rooms (Quản danh sách các phòng)
+- RoomTypes (Quản lý các loại phòng)
+- Booking (các đơn đặt phòng)
+-------------------------------
+Tự suy nghĩ các trường và kiểu dữ liệu cần thiết
+Code cách tạo các table trên bằng lệnh SQL
+*/
