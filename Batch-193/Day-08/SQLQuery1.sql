441711a1cd2c707d26f44640d9548cc8b662c2c1
nhannn87dn <nhannn87dn@gmail.com>
2025-05-15 20:56:54 +0700

    day8

---
+++ Batch-193/Day-08/SQLQuery1.sql
@@ -0,0 +1,86 @@
+﻿--SESION 14
+--TRANSATION ?
+/*
+Ví dụ: chuyển tiền từ người A sang B trong giao dịch Banking
+
+1. Trừ tiền người a
+2. Ghi log lịch sử giao dịch trừ tiền của a
+3. Cộng tiền cho b
+4. Ghi log lịch sử giao dịch cộng tiền cho b
+
+===========kết quả=======
+1. Nếu thành công --> 4 bước trên phải thành công
+2. Thất bại --> 1 trong 4 bước trên ko thực hiện được.
+
+*/
+
+
+--Mô phỏng chuyển tiền ngân hàng từ người a, sang người b
+
+--Tạo table bank
+CREATE TABLE bank
+(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(20),
+    balance DECIMAL(10,2)
+)
+--Ghi log giao dich
+CREATE TABLE bank_log
+(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    note NVARCHAR(500)
+)
+
+
+---Để tạo ra một transaction
+BEGIN TRANSACTION -- BEGIN TRAN ==> điểm bắt đầu
+	--Tất cả các câu lệnh truy vấn INSERT, UPDATE, DELETE, SELECT
+	INSERT bank
+		(name,balance)
+	VALUES
+		('b', 0);
+---Kết thúc với 1 trong 2 cách
+--ví dụ cách 1:
+COMMIT TRANSACTION --> xác nhận hoàn tất giao dịch
+--ví dụ cách 2:
+--ROLLBACK TRANSACTION --> Thì nó hủy bỏ giao dịch và hồi lại các lệnh truy vấn
+BẺGFI
+SELECT * FROM bank_log
+
+
+SELECT * FROM bank
+
+-- Mô phỏng giao dịch chuyển tiền banking a-> b
+BEGIN TRANSACTION;
+BEGIN TRY
+	-- Cố găng thực hiện giao dịch
+	--1. Trừ tiền người a 50USD
+	UPDATE bank SET balance = balance - 50 WHERE name = 'a';
+	--2. Ghi log lịch giao dịch trừ tiền của a
+	INSERT bank_log (note) VALUES (N'Chuyển từ a sang b 50 USD');
+	--3. Cộng tiền cho b
+	UPDATE bank SET balance = balance + 50 WHERE name = 'b';
+	--4. Ghi log lịch giao dịch trừ tiền của a
+	--INSERT bank_log (note) VALUES (N'B nhận tiền từ a - 50 USD');
+	INSERT bank_log (id, note) VALUES (10, N'B nhận tiền từ a - 50 USD');
+	--Nếu 4 queries trên thành công thì commit để hoàn tất giao dịch
+	COMMIT TRANSACTION;
+END TRY
+--Nếu giao dịch lỗi thì lỗi được catch
+BEGIN CATCH
+	-- Xử lý lỗi
+    SELECT
+        ERROR_NUMBER() AS ErrorNumber,
+        ERROR_MESSAGE() AS ErrorMessage;
+
+    -- Nếu có lỗi, hủy bỏ transaction
+    ROLLBACK TRANSACTION
+END CATCH
+
+
+-- Giao dịch bán hàng, có thanh toán online
+-- A. giao thành công
+-- 1. chọn sản phẩm --> tạo đơn hàng
+-- 2. Thanh toán
+-- 3. Cập nhật đơn hàng --> đã thanh toán thành công
+-- 4. Trừ số lượng tồn kho của các sp mà đơn hàng đã mua
---
+++ Batch-193/Day-08/SQLQuery3.sql
@@ -0,0 +1,123 @@
+﻿BEGIN TRAN
+
+USE Batch193_Data
+GO
+UPDATE bank SET balance = balance + 50
+WHERE name = 'a'
+GO
+-- chua commit
+COMMIT
+
+--SESSION 12 - TRIGGERS
+-- After Trigger -- Xảy ra sau khi sự kiện insert, update, delete
+
+CREATE OR ALTER TRIGGER dbo.trg_insertBankLog
+ON bank -- gắn cho table bank
+AFTER  UPDATE -- Khi bạn update table bank, thì trigger được kích hoạt và chạy
+AS
+BEGIN
+    INSERT bank_log (note) VALUES (CONCAT('bank log', GETDATE()));
+END;
+
+--
+SELECT * FROM bank_log
+SELECT * FROM bank
+UPDATE bank SET balance = balance + 50 WHERE name ='a'
+
+--Kết luận: Khi mà bạn thực hiện một sự kiện và đồng bạn muốn thực hiện
+-- luôn những tác vụ liên quan --> sử dụng trigger để chạy ngầm
+
+-- Ví dụ trong quy trình bán hàng
+SELECT * FROM orders
+-- Khi mà thanh toán thành công --> tự động kích hoạt trigger --> giảm số lượng tồn kho
+
+
+CREATE TRIGGER trg_Orders_Prevent_UpdateDelete
+ON orders
+AFTER UPDATE, DELETE -- Ngăn cách nhau bởi dấy phẩu khi có nhiều action
+AS
+BEGIN
+    IF EXISTS (SELECT * FROM inserted WHERE [order_status] = 4)
+    BEGIN
+        PRINT 'Cannot update order having status = 4 (COMPLETED).'
+        ROLLBACK -- Hủy lệnh UPDATE trước đó vào orders
+    END
+
+    IF EXISTS (SELECT * FROM deleted WHERE [order_status] = 4)
+    BEGIN
+        PRINT 'Cannot delete order having status = 4 (COMPLETED).'
+        ROLLBACK -- Hủy lệnh DELETE trước đó vào orders
+    END
+END;
+
+UPDATE orders SET order_note = 'babababa' WHERE order_id = 1
+
+
+CREATE TRIGGER trg_bank_log_PreventInsert
+ON bank_log
+INSTEAD OF INSERT
+AS
+BEGIN
+    PRINT 'Cannot insert data into the bank_log table.'
+END
+
+INSERT bank_log (note) VALUES (CONCAT('bank log 11111', GETDATE()));
+
+
+CREATE TRIGGER trg_prevent_table_modifications
+ON DATABASE
+FOR DROP_TABLE, ALTER_TABLE
+AS
+BEGIN
+    DECLARE @EventData XML = EVENTDATA()
+    DECLARE @ObjectName NVARCHAR(MAX)
+
+    -- Lấy tên bảng từ EVENTDATA
+    SET @ObjectName = @EventData.value('(/EVENT_INSTANCE/ObjectName)[1]', 'NVARCHAR(MAX)')
+
+    -- Kiểm tra nếu tên bảng là bảng cụ thể cần bảo vệ
+    IF @ObjectName = 'basket_a'
+    BEGIN
+        RAISERROR ('Không được phép xóa hoặc sửa đổi bảng basket_a.', 16, 1)
+        ROLLBACK
+    END
+END
+-- Thực hiện lệnh thì báo lỗi
+DROP table basket_a
+
+ALTER TABLE basket_a
+ALTER COLUMN fruit_a VARCHAR(60) NOT NULL
+
+
+CREATE TRIGGER trg_debug_inserted_deleted
+ON bank
+AFTER UPDATE
+AS
+BEGIN
+    -- Hiển thị giá trị mới (INSERTED)
+    SELECT * FROM INSERTED;
+
+    -- Hiển thị giá trị cũ (DELETED)
+    SELECT * FROM DELETED;
+
+    -- Log ví dụ
+    INSERT INTO bank_log (note)
+    SELECT 
+        CONCAT('Debug - New Balance: ', INSERTED.balance, ', Old Balance: ', DELETED.balance)
+    FROM INSERTED
+    INNER JOIN DELETED
+    ON INSERTED.id = DELETED.id;
+END;
+
+SELECT * FROM bank_log
+SELECT *FROM bank
+UPDATE bank SET balance = balance + 50 WHERE name ='a'
+
+bank_log = [
+	{id: 1, note: "nanskjs"}
+	{id: 2, note: "nanskjs", abc: "dsd"}
+];
+
+SELECT * FROM bank_log
+ALTER TABLE bank_log
+ADD abc VARCHAR(10) NULL DEFAULT NULL
