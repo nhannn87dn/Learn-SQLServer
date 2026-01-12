23888f944c094f01da844cb3e257405db6d9786c
nhannn87dn <nhannn87dn@gmail.com>
2026-01-12 19:43:58 +0700

    up

---
+++ Batch-195/day10.sql
@@ -0,0 +1,111 @@
+﻿ CREATE TABLE People (
+      ID INT PRIMARY KEY,
+      Info NVARCHAR(MAX)
+  )
+
+
+  DECLARE @info NVARCHAR(MAX)
+  SET @info = N'{
+    "firstName": "Nguyễn",
+    "lastName": "Thảo",
+    "age": 25,
+    "address": {
+      "StreetAddress": "290/58 Nơ Trang Long",
+      "City": "Việt Nam",
+      "State": "VN",
+      "postalCode": "76000"
+    },
+    "PhoneNumber": [
+      {"type": "home","number": "212 555-1234"},
+      {"type": "fax","number": "646 555-4567"}
+    ]
+  }'
+
+
+
+  SELECT JSON_MODIFY(@info, '$.age', '30') AS age
+
+  UPDATE People
+	SET Info = JSON_MODIFY(Info, '$.age', 36)
+	WHERE ID = 1
+
+
+  INSERT INTO People (ID, Info) VALUES (1, @info)
+
+  SELECT * FROM People
+
+
+SELECT
+  JSON_VALUE(Info, '$.age') AS age
+FROM People
+WHERE ISJSON(Info) > 0
+
+
+SELECT JSON_MODIFY('{"name": "John", "age": 30}', '$.name', 'Jane') AS name
+
+
+
+SELECT JSON_VALUE('{"name": "John", "age": 30}', '$.name') AS name
+
+
+SELECT JSON_QUERY('{"name": "John", "age": 30, "address": {"street": "123 Main St.", "city": "New York"}}', '$.address') AS address
+
+
+SELECT * FROM customers
+
+
+-- Tạo cấu trúc bảng customer_index
+CREATE TABLE dbo.customers_index (
+	[customer_id] [int]  NOT NULL,
+	[first_name] [nvarchar](255) NOT NULL,
+	[last_name] [nvarchar](255) NOT NULL,
+	[phone] [varchar](25) NOT NULL,
+	[email] [varchar](150) NOT NULL,
+	[birthday] [date] NULL,
+	[street] [nvarchar](255) NOT NULL,
+	[city] [nvarchar](50) NOT NULL,
+	[state] [nvarchar](50) NOT NULL,
+	[zip_code] [varchar](5) NULL,
+);
+
+
+INSERT INTO dbo.customers_index
+SELECT [customer_id], [first_name], [last_name], [phone], [email],
+       CONVERT(date, [birthday], 103), [street], [city], [state], [zip_code]
+FROM dbo.customers ORDER BY [birthday],[first_name];
+
+DELETE FROM dbo.customers_index
+
+SELECT * FROM customers_index
+
+
+SELECT customer_id FROM dbo.customers_index WHERE customer_id = 5
+--BEFORE
+-- CPU: 0.001668			---  0.0001581
+-- Rows: 1445				--- 1
+-- Cost: 0.0315382 (100%)   --- 0.0032831 (100%)
+-- Action: table scan		--- Clustered index
+
+--Tạo clustered index
+CREATE CLUSTERED INDEX CIX_customers_index_id
+ON customers_index (customer_id ASC);
+
+
+SELECT customer_id, phone FROM dbo.customers_index WHERE phone = '0968411372'
+
+CREATE UNIQUE NONCLUSTERED INDEX UIX_customer_index_phone 
+ON customers_index (phone)
+INCLUDE (first_name, last_name)
+
+
+SELECT customer_id, phone, first_name, last_name FROM dbo.customers_index WHERE phone = '0968411372'
+
+
+SELECT 
+*
+FROM products
+WHERE
+	category_id = 1,
+	brand_id = 1,
+	recomend=1, --1 la new
+	product_name LIKE '%s21%'
---
+++ Batch-195/day9.sql
@@ -0,0 +1,170 @@
+﻿CREATE VIEW v_view_products
+AS 
+SELECT
+	product_id,
+	product_name,
+	model_year,
+	discount,
+	description
+FROM
+	products
+
+--> v_products --> được gọi 1 table ảo
+
+--Nếu là 1 table thì có thể làm gì ?
+--SELECT
+SELECT * FROM v_products
+-- UPDATE
+UPDATE v_products SET discount = 0.2 WHERE product_id = 1
+-- INSERT , DELETE
+
+CREATE VIEW v_AllProducts
+WITH SCHEMABINDING
+AS (
+SELECT 
+ p.product_id,
+p.product_name,
+p.model_year,
+p.discount,
+p.description,
+ c.category_name,
+ b.brand_name
+FROM products AS p
+LEFT JOIN categories AS c ON c.category_id = p.category_id
+LEFT JOIN brands AS b ON b.brand_id = p.brand_id)
+
+
+SELECT * FROM v_AllProducts
+
+DROP VIEW IF EXISTS v_products
+
+--Tác dụng view là ? Bảo mật thông tin nhạy cảm có ở trong table gốc
+
+
+SELECT * FROM sys.views
+
+
+
+ALTER VIEW dbo.v_daily_sales
+WITH SCHEMABINDING, ENCRYPTION -- ràng buộc cấu trúc với các table tham chiếu
+AS
+SELECT
+    year(order_date) AS y,
+    month(order_date) AS m,
+    day(order_date) AS d,
+    p.product_id,
+    p.product_name,
+    p.discount,
+    (i.quantity * i.price) AS sales
+FROM
+    dbo.orders AS o
+INNER JOIN dbo.order_items AS i
+    ON o.order_id = i.order_id
+INNER JOIN dbo.products AS p
+    ON p.product_id = i.product_id;
+
+
+SELECT * FROM dbo.v_daily_sales;
+
+EXEC sp_helptext [v_daily_sales];
+
+
+EXEC sp_helptext [v_view_products];
+
+EXEC sp_rename 'products.discount', 'discounts', 'COLUMN';
+
+
+
+CREATE VIEW dbo.v_daily_sales_check
+AS
+SELECT
+    p.product_id,
+    p.product_name,
+    p.discount,
+    year(order_date) AS y,
+    month(order_date) AS m,
+    day(order_date) AS d,
+    (i.quantity * i.price) AS sales
+FROM
+    dbo.orders AS o
+INNER JOIN dbo.order_items AS i
+    ON o.order_id = i.order_id
+INNER JOIN dbo.products AS p
+    ON p.product_id = i.product_id
+WHERE p.discount > 0.05 -- Nếu không thõa mãn WHERE thì VIEW sẽ không chạy được
+WITH CHECK OPTION;
+
+SELECT * FROM v_daily_sales_check
+
+UPDATE v_daily_sales_check SET discount = 0.01 WHERE product_id = 20;
+
+UPDATE products SET discount = 0.01 WHERE product_id = 20;
+
+
+
+
+
+--1 kế hoạch thực thi
+
+
+CREATE PROCEDURE usp_dailysale -- đặt tên với prefix usp_
+AS
+BEGIN
+    BEGIN TRY
+        SELECT
+			p.product_id,
+			p.product_name, p.discount,
+			year(order_date) AS y,
+			month(order_date) AS m,
+			day(order_date) AS d,
+			(i.quantity * i.price) AS sales
+		FROM
+			dbo.orders AS o
+		INNER JOIN dbo.order_items AS i
+			ON o.order_id = i.order_id
+		INNER JOIN dbo.products AS p
+			ON p.product_id = i.product_id;
+    END TRY
+    BEGIN CATCH
+        -- Nếu có lỗi xảy ra, hiển thị thông tin lỗi
+        SELECT
+            ERROR_NUMBER() AS ErrorNumber,
+            ERROR_MESSAGE() AS ErrorMessage;
+        --Ném lỗi
+        THROW;
+    END CATCH;
+END;
+
+EXEC dbo.usp_dailysale
+
+CREATE PROC udsp_getOrderByDates(
+ @StartDate Date,
+ @EndDate Date
+)
+AS
+BEGIN
+	SELECT * FROM orders
+	WHERE order_date BETWEEN @StartDate AND @EndDate
+END
+
+
+EXEC dbo.udsp_getOrderByDates '2016-02-01', '2016-02-28' 
+
+
+
+CREATE PROCEDURE usp_TotalOrderByRangeDate (
+    @FromDate DATETIME, --tham số đầu vào
+    @ToDate DATETIME, --tham số đầu vào
+    @Total INT OUTPUT --Tham số đầu ra OUTPUT
+)
+AS
+BEGIN
+  SELECT @Total = COUNT(*) FROM orders WHERE CAST(order_date AS DATE)  BETWEEN @FromDate AND @ToDate
+END;
+
+
+DECLARE @TotalOrders INT;
+
+EXEC usp_TotalOrderByRangeDate '2016-01-01', '2016-01-30', @TotalOrders OUTPUT;
+
+SELECT @TotalOrders as TotalOrders;
